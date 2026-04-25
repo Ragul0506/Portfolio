c41d954940c4a12d1fe79bd935f71ed013d20aee
RAGULRAJ CHANDRASEKAR <sarwanragul65@gmail.com>
2026-04-25 14:59:27 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'motion': ['framer-motion'],
+          'gateway': ['./src/components/Gateway/GatewayVault'],
+          'projects': [
+            './src/components/Projects/ProjectsSection',
+            './src/components/Projects/CargoModal',
+            './src/components/Projects/HRMSModal',
+          ],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
+});
